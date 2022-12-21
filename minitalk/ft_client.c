544f6277f305ba44ce789e0a494a5ffc93999661
Zakariya HAMDOUCHI <zhamdouc@e1r5p22.clusters.42paris.fr>
2022-12-21 20:47:54 +0100

    minitalk1version

--- minitalk/ft_client.c
+++ minitalk/ft_client.c
@@ -1,40 +1,91 @@
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_client.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zhamdouc <zhamdouc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/21 12:29:50 by zhamdouc          #+#    #+#             */
+/*   Updated: 2022/12/21 18:10:05 by zhamdouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(int argc, char *argv[]) {
-  // Vérifier les arguments de la ligne de commande
-  if (argc != 3) {
-    printf("Usage : %s <PID du serveur> <message>\n", argv[0]);
-    return 1;
-  }
+#include "minitalk.h"
 
-  // Récupérer le PID du serveur et la chaîne de caractères à envoyer
-  pid_t server_pid = atoi(argv[1]);
-  char *msg = argv[2];
+unsigned int bits = 0;
 
-  // Envoyer un signal SIGUSR1 au serveur pour l'informer de la connexion
-  if (kill(server_pid, SIGUSR1) != 0) {
-    perror("Erreur lors de l'envoi du signal SIGUSR1");
-    return 1;
-  }
-
-  // Attendre la réponse du serveur
-  sleep(1);
-
-  // Envoyer la chaîne de caractères au serveur en utilisant un signal SIGUSR2
-  union sigval sval;
-  sval.sival_ptr = (void*) msg;
-  if (kill(server_pid, SIGUSR2) != 0) {
-    perror("Erreur lors de l'envoi du signal SIGUSR2");
-    return 1;
-  }
-
-  // Attendre la fin de la transmission de la chaîne de caractères
-  sleep(1);
-
-  return 0;
+void    ft_end(int signum)
+{
+	if (signum == SIGUSR1)
+		ft_printf("%d bits successfully sent !\n ", bits);
 }
 
+void    ft_send_bit(int pid, char octet)
+{
+	int i;
+
+	i = 0;
+	while (i < 8)
+	{
+		if (octet & 128)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				error(2);
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				error(2);
+		}
+		octet = octet << 1;
+		usleep(400);
+		i++;
+	}
+}
+
+void    error(int i)
+{
+	if (i == 1)
+	{
+		ft_printf("Usage: ./client [PID] [MESSAGE]");
+		exit(EXIT_FAILURE);
+	}
+	if (i == 2)
+	{
+		ft_printf("Error: kill() failed");
+		exit(EXIT_FAILURE);
+	}
+	if (i == 3)
+	{
+		ft_printf("Error: sigaction() failed");
+		exit(EXIT_FAILURE);
+	}
+	if (i == 4)
+	{
+		ft_printf("Error: malloc() failed");
+		exit(EXIT_FAILURE);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+	int pid;
+	struct sigaction act;
+
+	i = 0;
+	if (argc != 3)
+		error(1);
+	act.sa_handler = ft_end;
+	pid = atoi(argv[1]);
+	if (sigaction(SIGUSR1, &act, NULL) == -1)
+		error(3);
+	while (argv[2][i])
+	{
+		ft_send_bit(pid, argv[2][i]);
+		i++;
+		bits = bits + 8;
+	}
+	ft_send_bit(pid, '\0');
+	return (0);
+ }
