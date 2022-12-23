e3858deb03236e26bac87d876d4fe75f2cad821f
Zheylkoss <100791060+Zheylkoss@users.noreply.github.com>
2022-12-23 18:55:21 +0100

    signal

--- minishell/srcs/minishell.c
+++ minishell/srcs/minishell.c
@@ -6,23 +6,64 @@
 /*   By: zakariyahamdouchi <zakariyahamdouchi@st    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 16:18:12 by zakariyaham       #+#    #+#             */
-/*   Updated: 2022/12/23 16:51:41 by zakariyaham      ###   ########.fr       */
+/*   Updated: 2022/12/23 18:52:38 by zakariyaham      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-
-void prompt(char **env)
+void handle_sig(int sig)
 {
-
+	if (sig == SIGINT)//ctrl+c
+	{
+		printf("\n\nMinishell >");
+	}
+	if (sig == SIGQUIT)//ctrl+\
+		return ;
+	if (sig == SIGTSTP)//ctrl+z
+		exit(0);
 }
+
+//ctrl-D correspond a NULL, donc si l'utilisateur appuie sur ctrl-D sans rien ecrire readline sera egale a NULL
+//ctrl+d correspond a EOF
+/*
+char *command = readline();  // utiliser readline pour lire une ligne de commande
+if (command == NULL)  // si l'utilisateur appuie sur ctrl-D
+{
+    printf("\n");
+    exit(0);
+}
+*/
 
 int main(int argc, char **argv, char **env)
 {
+	struct sigaction sa;
+
+	sa.sa_handler = handle_sig;
+	sigemptyset(&sa.sa_mask);// utiliser sigaddset(&sa.sa_mask, SIGTSTP); si on souhaite bloque un signal en particulier
+	sa.sa_flags = 0;
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+	{
+		perror("sigaction failed");
+		return (1);
+	}
+	if (sigaction(SIGQUIT, &sa, NULL) == -1)
+	{
+		perror("sigaction failed");
+		return (1);
+	}
+	if (sigaction(SIGTSTP, &sa, NULL) == -1)
+	{
+		perror("sigaction failed");
+		return (1);
+	}
 	while (1)
 	{
-		//affichage du prompt a chaque fois
-		prompt(env);
+		pause();
 	}
+	return (0);
 }
