5e3b03d8fbd3351a4564702bcae59d4b98241613
sofiane belabbas <sofianebelabbas93220@gmail.com>
2023-02-24 14:31:18 +0100

    gestion exit on fork pas que si exit na pas de redirection

--- version officiel/src/pipex.c
+++ version officiel/src/pipex.c
@@ -22,6 +22,33 @@ int	dup_fd(int new_stdin, int new_stdout)
 	return (1);
 }
 
+static	int	check_no_sep(t_input_output	*output)
+{
+	return (find_if_have_output(output, ">") != 1 && find_if_have_output(output, "<") != 1 && find_if_have_output(output, ">>") != 1 && find_if_have_output(output, "<<") != 1);
+
+}
+
+int	pipex(t_maillons *maillons, char ***env, t_garbage *garbage)
+{
+	int	len;
+	int	wstatus;
+
+	wstatus = 0;
+	len = ft_strlen_maillons(maillons);
+	if (len == 0)
+		return (0);
+	if (len == 1)
+	{
+		if (check_no_sep(maillons->output) &&str_cmp(maillons->command, "exit") && check_builtin(maillons->args) == 0)
+			do_exit(maillons->args[1], garbage, 0);
+		else
+			pipex_one(maillons, env, garbage);
+	}
+	else if (len != 1)
+		pipex_multiple(len, garbage, 0, wstatus);
+	return (0);
+}
+/*
 int	pipex(t_maillons *maillons, char ***env, t_garbage *garbage)
 {
 	int	len;
@@ -48,4 +75,4 @@ int	pipex(t_maillons *maillons, char ***env, t_garbage *garbage)
 	else if (len != 1)
 		pipex_multiple(len, garbage, 0, wstatus);
 	return (0);
-}
+}*/
