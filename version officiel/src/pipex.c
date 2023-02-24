04428931d70965078aef9cdc07f9745341b453e8
sofiane belabbas <sofianebelabbas93220@gmail.com>
2023-02-24 18:54:45 +0100

    ahmet me met la pression

--- version officiel/src/pipex.c
+++ version officiel/src/pipex.c
@@ -27,7 +27,7 @@ static	int	check_no_sep(t_input_output	*output)
 	return (find_if_have_output(output, ">") != 1 && find_if_have_output(output, "<") != 1 && find_if_have_output(output, ">>") != 1 && find_if_have_output(output, "<<") != 1);
 
 }
-
+/*
 int	pipex(t_maillons *maillons, char ***env, t_garbage *garbage)
 {
 	int	len;
@@ -47,7 +47,7 @@ int	pipex(t_maillons *maillons, char ***env, t_garbage *garbage)
 	else if (len != 1)
 		pipex_multiple(len, garbage, 0, wstatus);
 	return (0);
-}
+}*/
 /*
 int	pipex(t_maillons *maillons, char ***env, t_garbage *garbage)
 {
@@ -74,3 +74,29 @@ int	pipex(t_maillons *maillons, char ***env, t_garbage *garbage)
 	return (0);
 }
 */
+int	pipex(t_maillons *maillons, char ***env, t_garbage *garbage)
+{
+	int	len;
+	int	wstatus;
+
+	wstatus = 0;
+	len = ft_strlen_maillons(maillons);
+	if (len == 0)
+		return (0);
+	if (len == 1)
+	{	
+		if (check_no_sep(maillons->output) && (str_cmp(maillons->command, "exit"))  && check_builtin(maillons->args) == 0)
+			do_exit(maillons->args[1], garbage, 0);
+		else if (find_if_have_output(maillons-> output, ">") != 1 && find_if_have_output(maillons -> output, "<") != 1 && (check_echo(maillons->args, 0, 0, 1) == 0 || check_builtin(maillons->args) == 0))
+		{
+			check_if_builtin(garbage->new_env, \
+			&(garbage->new_env), 0, garbage);
+			check_echo(garbage->maillons->args, 0, 0, 0);
+		}
+		else
+			pipex_one(maillons, env, garbage);
+	}
+	else if (len != 1)
+		pipex_multiple(len, garbage, 0, wstatus);
+	return (0);
+}
